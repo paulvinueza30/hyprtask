b36ff4ee63bb8284b9b6ba2c859a115bff32c4a4
Paul Martin Vinueza <paulvinueza30@gmail.com>
2025-10-11 17:23:29 -0400

    logger now logs to hyprtask logfil to avoid filling the stdout of the tui

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -3,13 +3,25 @@ package logger
 import (
 	"log/slog"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
 var Log *slog.Logger
 
 func Init() {
-	handler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+	logDir := "logs"
+	if err := os.MkdirAll(logDir, 0755); err != nil {
+		panic("Failed to create logs directory: " + err.Error())
+	}
+
+	logFile := filepath.Join(logDir, "hyprtask.log")
+	file, err := os.OpenFile(logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		panic("Failed to open log file: " + err.Error())
+	}
+
+	handler := slog.NewTextHandler(file, &slog.HandlerOptions{
 		Level:     slog.LevelDebug,
 		AddSource: true,
 		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
--- internal/ui/model.go
+++ internal/ui/model.go
@@ -2,7 +2,10 @@ package ui
 
 import (
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/paulvinueza30/hyprtask/internal/viewmodel"
+
+	"github.com/paulvinueza30/hyprtask/internal/ui/components"
 )
 
 type Model struct {
@@ -11,10 +14,13 @@ type Model struct {
 
 	cursor      int // idk how im gonna get it but we'll see
 	displayData viewmodel.DisplayData
+	
+	width  int
+	height int
 }
 
 func NewModel(ddChan chan viewmodel.DisplayData, viewActChan chan viewmodel.ViewAction) *Model {
-	return &Model{displayDataChan: ddChan, viewActionChan: viewActChan}
+	return &Model{displayDataChan: ddChan, viewActionChan: viewActChan }
 }
 
 func (m *Model) Init() tea.Cmd {
@@ -28,12 +34,26 @@ func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case viewmodel.DisplayData:
 		m.displayData = msg
 		return m, m.listenToDisplayDataChan()
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+		return m, nil
 	}
+
 	return m, nil
 }
 
 func (m *Model) View() string {
-	return ""
+  styledHeader := components.HeaderStyle.
+        Width(m.width).
+        Align(lipgloss.Center)
+
+    // 2. Render the actual text using the configured style.
+    headerText := styledHeader.Render("HyprTask")
+
+    // For now, you can just return the header string.
+    // Later, you will use lipgloss.JoinVertical() to add body content.
+    return headerText
 }
 func (m *Model) listenToDisplayDataChan() tea.Cmd {
 	return func() tea.Msg {
